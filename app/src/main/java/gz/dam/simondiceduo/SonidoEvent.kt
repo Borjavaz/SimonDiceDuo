aedd5fd77fb32b97e3511b13cd58461e0bf2eb90
Borja <bdesaavazquez@danielcastelao.org>
2025-11-26 13:31:41 +0100

    Introducidos los sonidos de todos los colores, error y victoria.

--- app/src/main/java/gz/dam/simondiceduo/SonidoEvent.kt
+++ app/src/main/java/gz/dam/simondiceduo/SonidoEvent.kt
@@ -4,5 +4,7 @@ package gz.dam.simondiceduo
  * Eventos de sonido para la UI
  */
 sealed class SonidoEvent {
-
+    data class ColorSound(val colorInt: Int) : SonidoEvent()
+    object Error : SonidoEvent()
+    object Victory : SonidoEvent()
 }
