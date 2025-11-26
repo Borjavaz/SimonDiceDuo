a9a1336149a2a95671132db84676e0135026f97e
Borja <bdesaavazquez@danielcastelao.org>
2025-11-26 13:24:46 +0100

    creacion de los estados del juego

--- app/src/main/java/gz/dam/simondiceduo/GameState.kt
+++ app/src/main/java/gz/dam/simondiceduo/GameState.kt
@@ -4,5 +4,11 @@ package gz.dam.simondiceduo
  * Clase sellada que representa todos los estados posibles del juego
  */
 sealed class GameState {
+    object Inicio : GameState()
+    object Preparando : GameState()
+    object MostrandoSecuencia : GameState()
+    object EsperandoJugador : GameState()
+    object ProcesandoInput : GameState()
+    object SecuenciaCorrecta : GameState()
 
 }
